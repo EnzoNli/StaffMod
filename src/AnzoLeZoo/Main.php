06349c024c0ea40334a029f5e2d5e0b5150a6eb6
AnzoLeZoo <59515391+AnzoLeZoo@users.noreply.github.com>
2020-01-04 22:17:31 +0100

    Update Main.php

--- src/AnzoLeZoo/Main.php
+++ src/AnzoLeZoo/Main.php
@@ -7,6 +7,7 @@ use pocketmine\plugin\PluginBase;
 use AnzoLeZoo\Command\StaffModCommand;
 use AnzoLeZoo\Command\VanishCommand;
 use AnzoLeZoo\Task\VanishScheduler;
+use pocketmine\utils\TextFormat as TF;
 
 
 class Main extends PluginBase implements Listener{
@@ -16,6 +17,7 @@ class Main extends PluginBase implements Listener{
     public $freeze = [];
 
     function onEnable(){
+        $this->getLogger()->info(TF::GREEN . "Plugin par AnzoLeZoo");
     	$this->getServer()->getPluginManager()->registerEvents($this, $this);
     	$this->getServer()->getCommandMap()->register("staffmod", new StaffModCommand($this));
     	$this->getServer()->getCommandMap()->register("vanishstaffmod", new VanishCommand($this));
